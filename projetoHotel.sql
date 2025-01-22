448d97f863c17f7e14927c33e90955729215d338
eunicecris <113383693+EuniceCristina@users.noreply.github.com>
2025-01-22 20:47:41 -0300

    commit

---
+++ projetoHotel.sql
@@ -0,0 +1,38 @@
+create database db_projetoHotel;
+use db_projetoHotel;
+
+create table hospede(
+id int not null primary key auto_increment,
+nome varchar(50),
+cpf int,
+telefone varchar(20),
+email varchar(50)
+);
+
+create table quarto(
+id int not null primary key auto_increment,
+numero int,
+tipo varchar(50),
+preco float,
+capacidade int, 
+descricao text
+);
+
+create table reserva(
+id int not null primary key auto_increment,
+hos_id int,
+checkin date,
+ckeckout date,
+total float,
+FOREIGN KEY (hos_id) REFERENCES hospede(id));
+
+create table reserva_quarto (
+	id int not null primary key auto_increment,
+    res_id int not null,
+    qua_id int not null,
+    dataRes date not null,
+    status varchar(20) not null,
+    foreign key (res_id) references reserva(id),
+    foreign key (qua_id) references quarto(id)
+);
+
